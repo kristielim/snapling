f044550998d459d18f6807473a720c99a29cfe74
sambhav <sambhav.reddy@gmail.com>
2018-03-31 16:04:43 -0700

    from dataurl

--- app.py
+++ app.py
@@ -10,6 +10,17 @@ def normal():
 
 @app.route('/pic', methods=["POST"])
 def pic_to_word():
+	file = request.files["file"]
+	path_name = 'temp_pics/testpic.jpg'
+	file.save(path_name)
+	translation = get_translation(path_name, 'fr')
+	#translation has original word and translation, separated by a comma
+	print("translation",translation)
+	os.remove(path_name)
+	return translation
+
+@app.route('/picURL', methods=["POST"])
+def picURL_to_word():
 	dataurl = request.data[22:]
 	imgdata = base64.b64decode(dataurl)
 	path_name = 'temp_pics/testpic.jpg'
