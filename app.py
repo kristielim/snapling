2cb1fa29e99b51fe2004f309e4917f232bc63787
Kristie Lim <kristie.lim@gmail.com>
2018-03-31 18:27:20 -0700

    a lot of changes

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+import base64
+from translateImage import get_translation
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def normal():
+	return render_template('ind.html')
+
+@app.route('/picURL', methods=["POST"])
+def picURL_to_word():
+	dataurl = request.data[22:]
+	imgdata = base64.b64decode(dataurl)
+	path_name = 'temp_pics/testpic2.jpg'
+	with open(path_name, 'wb') as file:
+		file.write(imgdata)
+	translation = get_translation(path_name, 'fr')
+	#translation has original word and translation, separated by a comma
+	print("translation",translation)
+	os.remove(path_name)
+	return translation
+
+@app.route("/connie/<translation>")
+def con(translation):
+	print(translation)
+	original, translated = translation.split("_")
+	return render_template("TranslatePage.html", originalWord=original, translatedWord=translated)
---
+++ translateImage.py
@@ -0,0 +1,36 @@
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+from google.cloud import translate
+
+def get_translation(image_path, target_language):
+	# returns the description of an image
+	def detect_labels(path):
+	    client = vision.ImageAnnotatorClient()
+
+	    with io.open(path, 'rb') as image_file:
+	        content = image_file.read()
+
+	    image = types.Image(content=content)
+
+	    response = client.label_detection(image=image)
+	    labels = response.label_annotations
+	    return (labels[0].description)
+	# returns a the given text in English and the translation in target language, separated by a comma
+	def translate_text(target, text):
+	    # Instantiates a client
+	    translate_client = translate.Client()
+
+	    # Translates some text into Russian
+	    translation = translate_client.translate(
+	        text,
+	        target_language=target)
+
+	    return text + '_' + translation['translatedText']
+
+	#Actual calls to helpers    
+	text = detect_labels(image_path)
+	return translate_text(target_language, text)
